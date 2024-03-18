bbf24e4cbd4497eaf929e33f5da153c57b8142b1
Anilkumar Reddy <anilkreddy.a@gmail.com>
2024-03-18 19:03:48 -0400

    Added backend with MongoDB

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import router from './routes/routes';
+
+
+
+const PORT = 4000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const MONGO_URL = 'mongodb+srv://tech:OGcs7Kt7eeM6yUpI@cluster.bybg8x2.mongodb.net';
+// mongodb+srv://tech:OGcs7Kt7eeM6yUpI@cluster.bybg8x2.mongodb.net/
+// OGcs7Kt7eeM6yUpI
+// user: tech
+// DATA API Key: dy73j8CArgpMXvCcypFgkfYVaFacPdSLt6szFvW7CXiaNvLbP1cZmfvl7mSofbrz
+mongoose
+  .connect(MONGO_URL, {
+    dbName: 'mailbox',
+  })
+  .then(() => {
+    console.log('Database connected...');
+  })
+  .catch((error) => {
+    console.log('error : ', error);
+  });
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http:://localhost:${PORT}`);
+});
